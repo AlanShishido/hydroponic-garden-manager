cc7fde7f29a23d1ba01e2e35a3dce9dee7ac76fe
AlanShishido <51389025+AlanShishido@users.noreply.github.com>
2021-01-05 20:09:27 -0400

    Teste Classe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Pessoa(object):
     def _init_(
             self,
-            name: str = None,
+            name = str,
             address: str = None,
             current_location: str = None
     ):
@@ -9,39 +9,6 @@ class Pessoa(object):
         self.address: str = address
         self.current_location: str = current_location
 
-    def walk_to(self, destiny: str):
-        self.current_location = destiny
+alan:Pessoa = Pessoa(name="string")
 
-
-class Funcionario(Pessoa):
-    def _init_(
-            self,
-            matricula: str,
-            name: str = None,
-            address: str = None
-    ):
-        super(Funcionario, self)._init_(
-            name=name,
-            address=address
-        )
-        self.matricula: str = matricula
-
-
-luiz: Funcionario = Funcionario(
-    name='luiz',
-    address='address #1',
-    matricula='#1033143'
-)
-print(luiz.name)
-print(luiz.matricula)
-print(luiz.current_location)
-luiz.walk_to('fpf')
-print(luiz.current_location)
-
-cassio: Pessoa = Pessoa(
-    name='cassio',
-    address='home',
-    current_location='home'
-)
-print(cassio.name)
-print(cassio.current_location)
+print(alan.name)
