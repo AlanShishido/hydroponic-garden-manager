9041bff092434712d7e0974a764d4fd03c749771
AlanShishido <51389025+AlanShishido@users.noreply.github.com>
2021-01-11 18:53:05 -0400

    exemples constants choice

---
+++ src/choices.py
@@ -0,0 +1,13 @@
+# Variable constant to project
+
+'''
+Number  51xx to codig information comunication interprocess
+'''
+
+class INFO(object):
+    """
+    docstring
+    """
+    INFO_TESTING_CLASS = 5100
+    
+    pass
